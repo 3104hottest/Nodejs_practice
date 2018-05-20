cfca12cbb37be27dbbfe77d772d8a56a7c51cec7
Satoshi Hotta <3104hotta@gmail.com>
2018-05-20 16:07:01 +0900

    List 2-7.1

--- app.js
+++ app.js
@@ -6,15 +6,21 @@ var server = http.createServer(getFromClient);
 server.listen(3000);
 console.log('Server start!');
 
+// ここまでメインプログラム
+
 //createServerの処理
-function getFromClient(req,res){
-	request = req;
-	response = res;
+function getFromClient(request,response){
 	fs.readFile('./index.html','UTF-8',
-		(error,data)=>{
+			(error,data)=>{
+			console.log('1');
+			var content = data.
+			replace(/dummy_title/g, 'タイトルです').
+			replace(/dummy_content/g, 'これがコンテンツです。');
+			console.log('2');
 			response.writeHead(200, {'Content-Type': 'text/html'});
 			response.write(data);
 			response.end();
-		}
-	);
+			console.log('3');
+			}
+		   );
 }
