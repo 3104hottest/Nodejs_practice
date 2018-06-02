8d746d6fb79415d32eeab733bc9ee84de7e87c83
Satoshi Hotta <3104hotta@gmail.com>
2018-06-02 14:58:53 +0900

    List4-16

--- express-app/index.js
+++ express-app/index.js
@@ -6,7 +6,14 @@ app.engine('ejs',ejs.renderFile);
 app.use(express.static('public'));
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extende: false}));
+app.use(bodyParser.urlencoded({extended: false}));
+
+var data = {
+	'Taro':'taro@yamada',
+	'Hanako':'hanako@flower',
+	'Sachiko':'sachiko@happy',
+	'Ichiro':'ichiro@baseball',
+};
 
 // トップページ
 app.get('/', (req, res) => {
@@ -14,17 +21,8 @@ app.get('/', (req, res) => {
   res.render('index.ejs',{
 	  title: 'Index',
 	  content: msg,
+	  data:data
 	  });
-});
-
-// ※POST送信の処理
-app.post("/",(req, res) => {
-	var msg = 'This is Posted Page!<br>' + 'あなたは「<b>'  
-	+ req.body.message + '</b>」と送信しました。';
-	res.render('index.ejs',{
-		title: 'other',
-		content: msg,
-	});
 });
 
 var server = app.listen(3000, () => {
