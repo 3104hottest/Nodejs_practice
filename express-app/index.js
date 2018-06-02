e1c8687d9a5d0ec5a11eed355398ca6262723734
Satoshi Hotta <3104hotta@gmail.com>
2018-06-02 14:22:48 +0900

    List 4-12

--- express-app/index.js
+++ express-app/index.js
@@ -5,24 +5,25 @@ var app = express()
 app.engine('ejs',ejs.renderFile);
 app.use(express.static('public'));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extende: false}));
+
 // トップページ
 app.get('/', (req, res) => {
-  var msg = 'This is Express Page!<br>' + 'これは、スタイルシートを利用した例です。';
-	//index.ejsをレンダリングする
+  var msg = 'This is Index Page!<br>' + '※メッセージを書いて送信して下さい。';
   res.render('index.ejs',{
 	  title: 'Index',
 	  content: msg,
-	  link:{href: '/other', text:'※別のページに移動'}
 	  });
 });
 
-// ※otherページ
-app.get("/other",(req, res) => {
-	var msg = 'This is Other Page!<br>' + 'これは、用意された別のページです。';
+// ※POST送信の処理
+app.post("/",(req, res) => {
+	var msg = 'This is Posted Page!<br>' + 'あなたは「<b>'  
+	+ req.body.message + '</b>」と送信しました。';
 	res.render('index.ejs',{
 		title: 'other',
 		content: msg,
-		link:{href:'/', text:'※トップに戻る'}
 	});
 });
 
