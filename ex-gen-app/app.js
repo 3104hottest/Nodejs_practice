745fafa3bd81cb37d7d63e6292048233ad46b86b
Satoshi Hotta <3104hotta@gmail.com>
2018-06-09 11:40:17 +0900

    List 6-3

--- ex-gen-app/app.js
+++ ex-gen-app/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var validator = require('express-validator');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/ajax',ajax);
+app.use(validator());
 
 var session_opt = {
 	secret: 'keyboard cat',
--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -26,17 +26,39 @@ router.get('/',(req, res, next) => {
 router.get('/add', (req, res, next) => {
 	var data = {
 		title: 'Hello/Add',
-		content: '新しいコードを入力'
+		content: '新しいコードを入力',
+		form: {name:'', mail:'', age:0}
 	}
 	res.render('hello/add',data);
 });
 
 router.post('/add', (req, res, next) => {
-	var nm = req.body.name;
-	var ml = req.body.mail;
-	var ag = req.body.age;
-	db.run('insert into mydata (name, mail, age) values (?, ?, ?)', nm, ml, ag);
-	res.redirect('/hello');
+	var response = res;
+	req.check('name','NAMEは必ず入力して下さい。').notEmpty();
+	req.check('mail','MAILはメールアドレスを記入して下さい。').isEmail();
+	req.check('age','AGEは年齢(整数)を入力下さい。').isInt();
+	req.getValidationResult().then((result) => {
+		if(!result.isEmpty()) {
+			var res = '<ul class="error">';
+			var result_arr = result.array();
+			for(var n in result_arr) {
+				res += '<li>' + result_arr[n].msg + '</li>'
+			}
+			res += '</ul>';
+			var data = {
+				title: 'Hello/Add',
+				content: res,
+				form: req.body
+			}
+			response.render('hello/add',data);
+		} else {
+			var nm = req.body.name;
+			var ml = req.body.mail;
+			var ag = req.body.age;
+			db.run('insert into mydata (name, mail, age) values (?, ?, ?)', nm, ml, ag);
+			res.redirect('/hello');
+		}
+	});
 });
 
 router.get('/edit', (req, res, next) => {
@@ -65,5 +87,29 @@ router.post('/edit', (req, res, next) => {
 	db.run(q, nm, ml, ag, id);
 	res.redirect('/hello');
 });
+
+router.get('/delete', (req, res, next) => {
+	var id = req.query.id;
+	db.serialize(() => {
+		var q = "select * from mydata where id = ?";
+		db.get(q, [id], (err, row) => {
+			if (!err) {
+				var data = {
+					title: 'Hello/Delete',
+					content: 'id = ' + id + ' のレコードを削除:',
+					mydata:row
+				}
+				res.render('hello/delete', data);
+			}
+		});
+	});
+});
+
+router.post('/delete', (req, res, next) => {
+	var id = req.body.id;
+	var q  = "delete from mydata where id = ?";
+	db.run(q, id);
+	res.redirect('/hello');
+})
 
 module.exports = router;
